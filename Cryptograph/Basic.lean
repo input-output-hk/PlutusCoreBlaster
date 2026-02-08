9f122a8068a1e3c6fb45c5ef13d62dfe35de7383
RSoulatIOHK <romain.soulat@iohk.io>
2026-02-08 18:26:46 +0100

    correct basics

--- Cryptograph/Basic.lean
+++ Cryptograph/Basic.lean
@@ -1,5 +1,11 @@
 
+import Cryptograph.Blake2b
 import Cryptograph.BLS12_381
+import Cryptograph.Ed25519
 import Cryptograph.Integer
+import Cryptograph.Keccak
+import Cryptograph.Ripemd
+import Cryptograph.Secp256k1
 import Cryptograph.Sha2
+import Cryptograph.Sha3
 import Cryptograph.String
