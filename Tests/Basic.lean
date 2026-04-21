fe6d3d0ee5dcb9e4a4efd78d43f14ee81aa5968b
Mark Petruska <petruska.mark@gmail.com>
2026-04-21 12:15:01 +0200

    Fix Tests/Basic.lean

--- Tests/Basic.lean
+++ Tests/Basic.lean
@@ -1,4 +1,10 @@
 
+import Cryptograph.Blake2b.Blake2bTestVectors
+import Cryptograph.Keccak.Keccak256TestVectors
+import Cryptograph.Ripemd.Ripemd160TestVectors
+import Cryptograph.Sha2.Sha256TestVectors
+import Cryptograph.Sha2.Sha512TestVectors
+import Cryptograph.Sha3.Sha3_256TestVectors
 
 import PlutusCore.Bitwise.Tests
 import PlutusCore.Cbor.Tests
